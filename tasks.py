f70fc0479da597bdef2c89f09b88cccd5e41a8f1
mr-js <hello@mr-js.ru>
2024-09-13 22:21:48 +0300

    RC 3.0

--- tasks.py
+++ tasks.py
@@ -1,39 +1,33 @@
 from celery_config import app
 import aiohttp
-from lxml import html
-from secrets import token_hex, choice
 import asyncio
 
 
 @app.task
-def scrape_data(url, proxies):
-    return asyncio.run(fetch_data(url, proxies))
+def scrape_data(url, proxy, timeout=10):
+    return asyncio.run(fetch_data(url, proxy, timeout=timeout))
 
 
-async def fetch_data(url, proxies):
-    data = ''
+async def fetch_data(url, proxy, timeout):
+    data = None
     my_timeout = aiohttp.ClientTimeout(
-        total=10, # total timeout (time consists connection establishment for a new connection or waiting for a free connection from a pool if pool connection limits are exceeded) default value is 5 minutes, set to `None` or `0` for unlimited timeout
-        sock_connect=10, # Maximal number of seconds for connecting to a peer for a new connection, not given from a pool. See also connect.
-        sock_read=10 # Maximal number of seconds for reading a portion of data from a peer
+        total=timeout, # total timeout (time consists connection establishment for a new connection or waiting for a free connection from a pool if pool connection limits are exceeded) default value is 5 minutes, set to `None` or `0` for unlimited timeout
+        sock_connect=timeout, # Maximal number of seconds for connecting to a peer for a new connection, not given from a pool. See also connect.
+        sock_read=timeout # Maximal number of seconds for reading a portion of data from a peer
     )
 
     client_args = dict(
         trust_env=True,
         timeout=my_timeout
     )
-    for attempt in range(len(proxies)):
-        try:
-            async with aiohttp.ClientSession(**client_args) as session:
-                async with session.get(url, proxy=choice(proxies)) as response:
-                        if response.status == 200:
-                            content = await response.text()
-                            # tree = html.fromstring(content)
-                            # data = next(iter(tree.xpath('//body/text()')), '')
-                            data = content
-                            break
-                        else:
-                            raise Exception(f'Status code {response.status}')
-        except Exception as e:
-            continue
-    return data
+    try:
+        async with aiohttp.ClientSession(**client_args) as session:
+            async with session.get(url, proxy=proxy) as response:
+                    if response.status == 200:
+                        data = await response.text()
+                    else:
+                        raise Exception(f'Status code {response.status}')
+    except Exception as e:
+        pass
+    finally:
+        return data
--- test.py
+++ test.py
@@ -1,42 +1,91 @@
+import logging
 import asyncio
 import time
 from secrets import token_hex, choice
 from tasks import scrape_data
 import nodriver as uc
 import json
+import os, sys
 
 
-targets = [{'id': token_hex(4), 'name': f'Task {x}', 'url': 'https://api.ipify.org?format=json'} for x in range(8)]
-
-async def get_proxies():
-    url = 'https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text'
-    driver = await uc.start(browser_args=[f"--proxy-server=socks5://127.0.0.1:2080", '--lang=en',])
-    tab = await driver.get(url)
-    content = await tab.select('pre')
-    proxies = content.text.split('\n')
-    await tab.close()
-    return proxies
-
-proxies = asyncio.run(get_proxies())
-# print(proxies)
-
-async def fetch_data(target):
-    url = target['url']
-    task_id = target['id']
-    task_name = target['name']
-    task = scrape_data.delay(url=url, proxies=proxies)
-    while not task.ready():
-        await asyncio.sleep(1)
-    result = task.get(timeout=10)
-    print(f'Result for task #{task_id} "{task_name}" with URL {url}: {result}')
-    return {'id': task_id, 'id': task_name, 'result': result}
+class SuperScrapper:
+    proxies = {}
+    def __init__(self):
+        logging.basicConfig(
+            handlers=[
+                    logging.StreamHandler(),
+                    logging.FileHandler(f'{self.__class__.__name__}.log', 'w', 'utf-8')
+                    ],
+            format='%(asctime)s [%(funcName)s] %(levelname)s %(message)s',
+            datefmt='%Y.%m.%d %H:%M:%S',
+            level=logging.ERROR
+            )    
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(logging.DEBUG)
+        self.log.info('STARTED')
+        asyncio.run(self.load_proxies())
 
 
-async def run_all_tasks():
-    tasks = [fetch_data(target) for target in targets]
-    results = await asyncio.gather(*tasks)
-    print(f"All tasks completed. Results: {results}")
+    async def load_proxies(self):
+        try:
+            url = 'https://api.proxyscrape.com/v3/free-proxy-list/get?request=displayproxies&proxy_format=protocolipport&format=text'
+            driver = await uc.start(browser_args=[f"--proxy-server=socks5://127.0.0.1:2080", '--lang=en',])
+            tab = await driver.get(url)
+            content = await tab.select('pre')
+            self.proxies = content.text.split('\n')
+            await tab.close()
+            if len(self.proxies) > 0:
+                self.log.info(f'proxies updated OK: {len(self.proxies)}')
+            else:
+                raise Exception('cannot get proxies')
+        except Exception as e:
+            self.log.critical(e)    
+
+
+    async def fetch_data(self, target, timeout=10, precheck_proxy=False):
+        url = target['url']
+        task_id = target['id']
+        task_name = target['name']
+        for attempt in range(len(self.proxies)):
+            self.log.debug(f'{len(self.proxies)=}')
+            try:
+                proxy = choice(self.proxies)
+            except:
+                break
+            if precheck_proxy:
+                self.log.debug(f'{target=} with {proxy=}')
+                task = scrape_data.delay(url='https://api.ipify.org?format=json', proxy=proxy, timeout=timeout)
+                while not task.ready():
+                    await asyncio.sleep(1)
+                if not task.get(timeout=timeout):
+                    try:
+                        self.proxies.remove(proxy)
+                    except:
+                        pass
+                    continue
+            task = scrape_data.delay(url=url, proxy=proxy, timeout=timeout)
+            while not task.ready():
+                await asyncio.sleep(1)
+            result = task.get(timeout=timeout)
+            if not result:
+                try:
+                    self.proxies.remove(proxy)
+                except:
+                    pass
+                continue
+            self.log.debug(f'Result for task #{task_id} "{task_name}" with URL {url}: {result}')
+            break
+        return {'id': task_id, 'name': task_name, 'result': result}
+
+
+    async def run_all_tasks(self, targets):
+        tasks = [self.fetch_data(target) for target in targets]
+        results = await asyncio.gather(*tasks)
+        self.log.info(f"All tasks completed: {results=}")
 
 
 if __name__ == "__main__":
-    asyncio.run(run_all_tasks())
+    ss = SuperScrapper()
+    demo_targets_amount = 10
+    targets = [{'id': token_hex(4), 'name': f'Task {x}', 'url': 'https://api.ipify.org?format=json'} for x in range(demo_targets_amount)]
+    asyncio.run(ss.run_all_tasks(targets))
